e5ec8f0cb9168f9ee423c425243d9e28c30240e0
Mike Ries <mries92@gmail.com>
2021-06-05 11:42:59 -0400

    Mod info setup. Placeholder assets for pogo stick item.

--- src/main/java/bounce/Bounce.java
+++ src/main/java/bounce/Bounce.java
@@ -3,10 +3,12 @@ package bounce;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
+import net.minecraftforge.common.ForgeConfig;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -15,7 +17,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class Bounce {
     public static final String modId = "bounce";
     private static final DeferredRegister<Item> POGO_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, modId);
-    private static final RegistryObject<Item> POGO = POGO_REGISTRY.register("wooden_pogo_stick",  () -> new Item(new Item.Properties(){{tab(ItemGroup.TAB_TRANSPORTATION);}}));
+    private static final RegistryObject<Item> POGO = POGO_REGISTRY.register("wooden_pogo_stick",  () -> new Item(new Item.Properties().tab(ItemGroup.TAB_TRANSPORTATION).stacksTo(1).durability(100)));
 
     public Bounce() {
         POGO_REGISTRY.register(FMLJavaModLoadingContext.get().getModEventBus());
